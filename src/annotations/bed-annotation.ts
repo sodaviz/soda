a9b74385c997d20168ca9a69f58f5b6a1aef9845
Jack Roddy <jack.w.roddy@gmail.com>
2021-11-01 22:56:54 -0600

    Added BED3, 6, 9, 12 types
    I used the Omit<T, keys> typescript feature to define these types. It
    seems pretty slick, but we'll see if it causes any problems down the
    line.

--- src/annotations/bed-annotation.ts
+++ src/annotations/bed-annotation.ts
@@ -5,72 +5,151 @@ import { Orientation } from "./orientation";
  * A simple interface that holds the arguments for a BedAnnotation constructor.
  */
 export interface BedAnnotationConfig extends AnnotationConfig {
-  chrom?: string;
   /**
-   * The name of the record.
+   * A BED3 field that describes the chromosome of the record.
+   */
+  chrom: string;
+  /**
+   * A BED6 field that describes the name of the record.
    */
   name?: string;
   /**
-   * The score of the record.
+   * A BED6 field that describes the "score" of the record.
    */
   score?: number;
   /**
-   * The orientation of the record.
+   * A BED6 field that describes the orientation/strand of the record.
    */
   strand?: Orientation;
   /**
-   * A BED field that describes at which coordinate the feature should stop being drawn "thickly."
+   * A BED9 field that describes at which coordinate the feature should start being drawn "thickly."
    */
   thickStart?: number;
   /**
-   * The color that the feature should be drawn.
+   * A BED9 field that describes at which coordinate the feature should stop being drawn "thickly."
    */
   thickEnd?: number;
   /**
-   * The color that the feature should be drawn.
+   * A BED9 field BED field that defines the color of the feature. It is an RGB string, ie. (<0-256>, <0-256>, <0-256>).
    */
   itemRgb?: string;
+  /**
+   * A BED12 field for records that should be drawn as discontiguous/fragmented glyphs. This describes the number of
+   * fragments.
+   */
   blockCount?: number;
+  /**
+   * A BED12 field for records that should be drawn as discontiguous/fragmented glyphs. This describes the size of each
+   * fragment.
+   */
   blockSizes?: number[];
+  /**
+   * A BED12 field for records that should be drawn as discontiguous/fragmented glyphs. This describes the offset of
+   * each fragment.
+   */
   blockStarts?: number[];
 }
+
+export type Bed3Annotation = Omit<
+  BedAnnotation,
+  | "name"
+  | "score"
+  | "strand"
+  | "thickStart"
+  | "thickEnd"
+  | "itemRgb"
+  | "blockCount"
+  | "blockSizes"
+  | "blockStarts"
+>;
+
+export type Bed6Annotation = {
+  name: string;
+  score: number;
+  strand: Orientation;
+} & Omit<
+  BedAnnotation,
+  | "thickStart"
+  | "thickEnd"
+  | "itemRgb"
+  | "blockCount"
+  | "blockSizes"
+  | "blockStarts"
+>;
+
+export type Bed9Annotation = {
+  name: string;
+  score: number;
+  strand: Orientation;
+  thickStart: number;
+  thickEnd: number;
+  itemRgb: string;
+} & Omit<BedAnnotation, "blockCount" | "blockSizes" | "blockStarts">;
+
+export type Bed12Annotation = {
+  name: string;
+  score: number;
+  strand: Orientation;
+  thickStart: number;
+  thickEnd: number;
+  itemRgb: string;
+  blockCount: number;
+  blockSizes: number[];
+  blockStarts: number[];
+} & BedAnnotation;
 
 /**
  * An Annotation definition for BED records. For more information on BED records, see
  * https://genome.ucsc.edu/FAQ/FAQformat.html#format1.
  */
 export class BedAnnotation extends Annotation {
-  chrom?: string;
   /**
-   * The name of the record.
+   * A BED3 field that describes the chromosome of the record.
+   */
+  chrom: string;
+  /**
+   * A BED6 field that describes the name of the record.
    */
   name?: string;
   /**
-   * The score of the record.
+   * A BED6 field that describes the "score" of the record.
    */
   score?: number;
   /**
-   * The orientation of the record.
+   * A BED6 field that describes the orientation/strand of the record.
    */
   strand?: Orientation;
   /**
-   * A BED field that describes at which coordinate the feature should stop being drawn "thickly."
+   * A BED9 field that describes at which coordinate the feature should start being drawn "thickly."
    */
   thickStart?: number;
   /**
-   * The color that the feature should be drawn.
+   * A BED9 field that describes at which coordinate the feature should stop being drawn "thickly."
    */
   thickEnd?: number;
   /**
-   * The color that the feature should be drawn.
+   * A BED9 field BED field that defines the color of the feature. It is an RGB string, ie. (<0-256>, <0-256>, <0-256>).
    */
   itemRgb?: string;
+  /**
+   * A BED12 field for records that should be drawn as discontiguous/fragmented glyphs. This describes the number of
+   * fragments.
+   */
   blockCount?: number;
+  /**
+   * A BED12 field for records that should be drawn as discontiguous/fragmented glyphs. This describes the size of each
+   * fragment.
+   */
   blockSizes?: number[];
+  /**
+   * A BED12 field for records that should be drawn as discontiguous/fragmented glyphs. This describes the offset of
+   * each fragment.
+   */
   blockStarts?: number[];
 
   constructor(config: BedAnnotationConfig) {
     super(config);
+    this.chrom = config.chrom;
     if (config.strand) {
       this.strand = config.strand;
     } else {
