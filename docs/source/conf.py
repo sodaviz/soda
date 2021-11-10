6320273d12adfd2f9fa4b450fba968d5fbaa83c1
Jack Roddy <jack.w.roddy@gmail.com>
2021-11-10 15:25:33 -0700

    Removed some excess stuff from sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,11 +67,3 @@ html_static_path = ['_static']
 html_css_files = [
     'css/custom.css',
 ]
-
-html_js_files = [
-    "js/custom.js",
-]
-
-latex_elements = {
-  'extraclassoptions': 'openany,oneside'
-}
