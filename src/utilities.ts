669fbfecba4ad78d7d0c886d0ed5ff8a802ce79e
Jack Roddy <jack.w.roddy@gmail.com>
2021-11-18 11:36:37 -0700

    Added aggregateTransitive() function
    This function makes AnnotationGroups from a list of Annotations. It
    groups the Annotations by some arbitrary criteria, which is passed as a
    callback function.

--- src/utilities.ts
+++ src/utilities.ts
@@ -4,6 +4,6 @@ export {
   generateSequenceAnnotations,
   GenerationPattern,
 } from "./utilities/annotation-generation";
-export { condenseAnnotations } from "./utilities/condense";
+export { aggregateTransitive, condenseAnnotations } from "./utilities/condense";
 export { exportPng } from "./utilities/export-png";
 export { generateId } from "./utilities/id-generation";
--- src/utilities/condense.ts
+++ src/utilities/condense.ts
@@ -32,3 +32,46 @@ export function condenseAnnotations<A extends Annotation>(
   }
   return groups;
 }
+
+/**
+ * A utility function that aggregates Annotation objects into Annotation groups based off of the supplied criteria.
+ * This function assumes that your aggregation criteria is transitive, i.e. if criteria(a, b) = true and criteria(b,
+ * c) = true, then criteria(a, c) = true.
+ * @param annotations The annotations to aggregate.
+ * @param criteria The callback function that determines whether or not the aggregation criteria is true for any
+ * two given Annotations.
+ */
+export function aggregateTransitive<A extends Annotation>(
+  annotations: A[],
+  criteria: (a: A, b: A) => boolean
+): AnnotationGroup<A>[] {
+  let annCopy = annotations.slice();
+  let groups: AnnotationGroup<A>[] = [];
+
+  while (annCopy.length > 0) {
+    let group = new AnnotationGroup<A>({});
+    let groupRepresentative = annCopy[0];
+    group.add(groupRepresentative);
+    arraySwapRemove(annCopy, 0);
+    for (let idx = 0; idx < annCopy.length; idx++) {
+      if (criteria(groupRepresentative, annCopy[idx])) {
+        group.add(annCopy[idx]);
+        arraySwapRemove(annCopy, idx);
+        idx--;
+      }
+    }
+    groups.push(group);
+  }
+
+  return groups;
+}
+/**
+ * A utility function for removing an element from an array.
+ * @param arr The array to remove an element from.
+ * @param idx The index of the item to remove.
+ * @internal
+ */
+function arraySwapRemove(arr: any[], idx: number) {
+  arr[idx] = arr[arr.length - 1];
+  arr.pop();
+}
