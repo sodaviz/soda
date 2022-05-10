400baa593c19540c8d04ab5a06b6ff1d8eecf671
Jack Roddy <jack.w.roddy@gmail.com>
2022-05-10 12:40:19 -0600

    Fixed VerticalLayout exports and docs
    Both the exports and docs were missing... whoops!

--- src/layout.ts
+++ src/layout.ts
@@ -2,3 +2,4 @@ export { AnnotationGraph } from "./layout/annotation-graph";
 export { intervalGraphLayout } from "./layout/interval-graph-layout";
 export { greedyGraphLayout } from "./layout/greedy-graph-layout";
 export { heuristicGraphLayout } from "./layout/heuristic-graph-layout";
+export { VerticalLayout, MapVerticalLayout } from "./layout/vertical-layout";
--- src/layout/vertical-layout.ts
+++ src/layout/vertical-layout.ts
@@ -2,16 +2,32 @@ import { Annotation } from "../annotations/annotation";
 import { Chart } from "../charts/chart";
 import { GlyphCallback } from "../glyph-utilities/glyph-modifier";
 
+/**
+ * An interface that defines the object that Charts use to store the vertical layout of glyphs.
+ */
 export interface VerticalLayout {
+  /**
+   * This callback is used by default to place a glyph in a Chart's row.
+   */
   row: GlyphCallback<Annotation, Chart<any>, number>;
+  /**
+   * This value is used by default to set the height of a Chart to display the required number of rows.
+   */
   rowCount: number;
 }
 
+/**
+ * A placeholder VerticalLayout object--it just places every glyph in row 0.
+ */
 export const defaultVerticalLayout: VerticalLayout = {
   row: () => 0,
   rowCount: 1,
 };
 
+/**
+ * An extension of VerticalLayout that additionally has a Map. This object is returned by SODA's default layout
+ * functions, and the Map is used to build the row() callback.
+ */
 export interface MapVerticalLayout extends VerticalLayout {
   rowMap: Map<string, number>;
 }
